23cae5b0dc61c9ba42c6867ca90779ade4a0f69b
Marcus Karlsson <marcus.caarlsson92@gmail.com>
2019-06-29 18:23:32 +1200

    Parsing let and return statements.

---
+++ ast/ast.go
@@ -0,0 +1,52 @@
+package ast
+
+import "github.com/Tinee/monkey/token"
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Node interface {
+	TokenLiteral() string
+}
+type Statement interface {
+	Node
+	statementNode()
+}
+type Expression interface {
+	Node
+	expressionNode()
+}
